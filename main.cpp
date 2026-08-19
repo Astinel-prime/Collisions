f1174a794f2b225b1cef2c5ae18ea06417225982
Astinel <126586654+Astinel-prime@users.noreply.github.com>
2026-08-19 23:40:35 +0530

    ball collisions with random shapes
    Collisions with random shape incluided buggy collision though
    Balls still aren't colliding
    New Idea: Corner Collision Ball pops

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include<cmath>
 #include<random>
 
-
+#include<algorithm>
 
 #define Grass (Color) {76,175,80,255}
 #define Sky (Color) {100, 149, 237,255}
@@ -11,6 +11,12 @@
 
 using namespace std;
 const float root=sqrt(2);
+
+class Point{
+    public: 
+    float x,y;
+};
+
 
 
 class Line{
@@ -30,6 +36,33 @@ class Line{
 };
 
 Line l[4];
+
+class LineSeg{
+    public:
+    float l_x,l_y,r_x,r_y;
+    float normal_x,normal_y;
+    float len;
+
+    float dis_from_line(float x,float y){
+        return ((x-l_x)*normal_x)+((y-l_y)*normal_y);
+    }
+    
+    float tdis1(float x,float y){
+        return (x-l_x)*-normal_y+(y-l_y)*normal_x;
+    }
+    float tdis2(float x,float y){
+        return (x-r_x)*-normal_y+(y-r_y)*normal_x;
+    }
+    float speed_normal(float speed_x,float speed_y){
+        return speed_x*normal_x+speed_y*normal_y;
+    }
+    float speed_tangent(float speed_x,float speed_y){
+        return speed_x*normal_y-speed_y*normal_x;
+    }
+};
+vector<LineSeg> edge;
+
+
 class Ball{
     public:
     float x,y;
@@ -45,8 +78,9 @@ class Ball{
         
     for(int i=0;i<4;i++){
         float dis=l[i].dis_from_line(x,y);
+        if(dis>radius||dis<-radius) continue;
         if(dis<=radius){
-            float depth = radius-dis;
+            float depth = radius-abs(dis);
             x+=l[i].normal_x*depth;
             y+=l[i].normal_y*depth;
             if(l[i].speed_normal(speed_x,speed_y)<0){
@@ -59,6 +93,32 @@ class Ball{
             }
         }
     }
+    
+
+    for(int i=0;i<edge.size();i++){
+        LineSeg seg=edge[i];
+        float dis=seg.dis_from_line(x,y);
+        if(abs(dis)<=radius){
+            float t1=seg.tdis1(x,y),t2=seg.tdis2(x,y);
+            if(t1<=0 && t2>=0){
+                float depth=radius-abs(dis);
+                if(dis<0) depth=-depth;
+                x+=seg.normal_x*depth;
+                y+=seg.normal_y*depth;
+            
+            float s_n=seg.speed_normal(speed_x,speed_y);
+            float s_t=seg.speed_tangent(speed_x,speed_y);
+            if((dis>=0 && s_n<0)||(dis<0&&s_n>0)){
+                s_n=-s_n;
+                speed_x=s_n*seg.normal_x+s_t*seg.normal_y;
+                speed_y=s_n*seg.normal_y-s_t*seg.normal_x;
+            }
+        }
+        }
+    }
+    
+    
+    
         
         x+=speed_x;
         y+=speed_y;
@@ -69,18 +129,20 @@ class Ball{
 Ball ball;
 vector<Ball> b;
 
+
+
 int main(){
     cout<<"Start..."<<endl;
     const int screen_height=1000;
     const int screen_width=1000;
 
     InitWindow(screen_width,screen_height,"Collision Simulation");
-    SetTargetFPS(60);
+    SetTargetFPS(30);
    
   
     ball.x=500;
     ball.y=500;
-    ball.radius=20;
+    ball.radius=10;
     ball.speed_x=7;
     ball.speed_y=-5;
     Rectangle r={500,500+250*root,500,500};
@@ -107,6 +169,51 @@ int main(){
     l[3].normal_x=+1/root;
     l[3].normal_y=-1/root;
 
+    vector<Vector2> points,points1;
+    int j=0;
+    float last_x=320,last_y=380;
+    for(int i=0;i<50;i++){
+        float pos_x,pos_y=GetRandomValue(380,440);
+        
+        pos_x=last_x;
+        last_x+=3;
+    
+        Vector2 vec={pos_x,pos_y};
+        points.push_back(vec);
+        
+    }
+    for(int i=0;i<60;i++){
+        float pos_x=last_x,pos_y=GetRandomValue(455,520);
+        
+        last_x-=2;
+
+    
+        Vector2 vec={pos_x,pos_y};
+        points.push_back(vec);
+        points1.push_back(vec);
+        
+    }
+    
+    points.push_back(points[0]);
+    vector<LineSeg> edges;
+    for(int i=0;i<points.size()-1;i++){
+    
+        LineSeg ls;
+        ls.l_x=points[i].x;
+        ls.l_y=points[i].y;
+        ls.r_x=points[i+1].x;
+        ls.r_y=points[i+1].y;
+        ls.len=sqrt((points[i].x-points[i+1].x)*(points[i].x-points[i+1].x)+(points[i].y-points[i+1].y)*(points[i].y-points[i+1].y));
+
+
+        ls.normal_x=-(points[i+1].y-points[i].y)/ls.len;
+        ls.normal_y=(points[i+1].x-points[i].x)/ls.len;
+        edges.push_back(ls);
+        
+    }
+    edge=edges;
+    
+
     
     int t=0;
 
@@ -116,12 +223,33 @@ int main(){
         
         ball.Update();
 
+        
+
         for(int i=0;i<b.size();i++){
             b[i].Update();
         }
         ClearBackground(Grass);
         DrawRectanglePro(r1,v,225,WHITE);
         DrawRectanglePro(r,v , 225, Sky);
+ 
+        
+        
+        for(int i=0;i<9;i++){
+            vector<Vector2> parts;
+            parts.push_back(points[i]);
+            parts.push_back(points1[i]);
+            parts.push_back(points[i+1]);
+            DrawTriangleStrip(parts.data(),parts.size(),WHITE);
+          
+            parts.clear();
+            parts.push_back(points[i]);
+            parts.push_back(points1[i]);
+            parts.push_back(points[i+1]);
+            DrawTriangleStrip(parts.data(),parts.size(),WHITE);
+        }
+        
+  
+        
         ball.Draw();
         t++;
         if(t==120){
@@ -129,9 +257,9 @@ int main(){
             Ball balls;
             balls.x=500;
             balls.y=500-250;
-            balls.radius=20;
-            balls.speed_x=GetRandomValue(3,8);
-            balls.speed_y=GetRandomValue(3,8);
+            balls.radius=10;
+            balls.speed_x=GetRandomValue(3,4);
+            balls.speed_y=GetRandomValue(3,4);
             b.push_back(balls);
 
         }
